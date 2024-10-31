62a18e19dfdf5b787efc48dc60a2fff145b9b483
Chris Svajlenka <chris.svajlenka@ariessolutions.io>
2024-10-31 13:59:14 -0700

    mvp status

--- bot.py
+++ bot.py
@@ -1,33 +1,35 @@
 import discord
 import os
+from dotenv import load_dotenv
 
-client = discord.Client()
+
+client = discord.Client(self_bot=True)
+bot = None
+@client.event
+async def on_ready():
+    print(f'Logged in and ready')
 
 @client.event
 async def on_message(message):
-    if message.author == self.bot.user.id:
-        return
+    # if message.author == bot.user.id:
+    #     return
     print('Incoming message')
     print("guild: ",message.guild)
     print("author: ",message.author)
     print("channel: ",message.channel)
     print("content: ",message.content)
     channel = message.channel
-    if message.content.startswith('@Bump Reminder'):
-        await message.channel.send('/bump')
-        async for command in channel.slash_commands(query="bump"):
+    if message.content == '@Bump Reminder':
+        commands = await message.channel.application_commands()
+        print("commands",commands)
+        for command in commands:
+            print("command", command.name)
             if command.name == "bump":
-                break
-            print(f"command: {command}")
-            user = self.bot.get_user(message.user_id)
-            command = await command.__call__(channel=channel, user=user)
-    if message.content.startswith('!drp bump'):
-        async for command in channel.slash_commands(query="bump"):
-            if command.name == "bump":
-                break
-            print(f"command: {command}")
-            user = self.bot.get_user(message.user_id)
-            command = await command.__call__(channel=channel, user=user)
+                command_res = await command.__call__(channel=channel) #, user=command.user)
+                print(command_res)
 
-client.run(os.getenv("TOKEN"))
+load_dotenv()
+token = os.getenv("TOKEN")
+print("LOGGING IN WITH TOKEN: ", token)
+client.run(token)
 
